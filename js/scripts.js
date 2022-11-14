ecbd1bb5b90993d5179179c6e4817d6fa5fa93dc
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2022-11-14 09:33:09 +0100

    Crete pokemonList variable

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    {name: 'Pikachu', height: 4, types: ['electric']},
+    {name: 'Charmander', height: 6, types: ['fire']},
+    {name: 'Squirtle', height: 5, types: ['water']}
+];
