1f6ee5a64bce46301ff827bb696f2f4e82b8e660
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2022-11-15 15:23:36 +0100

    Added IIFE and changed code to comply with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,47 @@
-// list of pokemons with height and types
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        types: ['electric']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
+let pokemonRepository = (function(){
+    // list of pokemons with height and types
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['electric']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        }
+    ];
+
+    function add(item) {
+        pokemonList.push(item);
     }
-];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 //forEach loop to write out the pokemon names with heights
-pokemonList.forEach(function(pokemon){
+pokemonRepository = pokemonRepository.getAll(); // fetch pokemonList from the function
+
+//write out the pokemonRepository
+pokemonRepository.forEach(function(pokemon){
     document.write(pokemon.name + ' (Height: ' + pokemon.height + ' m)');
     if (pokemon.height > 0.6){
         document.write(' - Wow, that\'s big!');
